373e291bc5efdce1231600ee5ddb6d9d551f5a2b
Nataan' ॐ <natanluis2011@hotmail.com>
2021-01-12 18:06:37 -0300

    MainCOntext criado

--- WebStore.Infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WebStore.Infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ WebStore.Infra/MainContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain;
+namespace WebStore.Infra
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
