480166ea448143beb10688473695b7b77b4ecd00
Nataan' ॐ <natanluis2011@hotmail.com>
2021-01-18 16:25:31 -0300

    Criação e implementação da Configuração do Mapeamento da classe Fornecedor.
    Configuração das classes Program e Startup

---
+++ WebStore.Infra/Mappings/FornecedorMapConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebStore.Project.Backend.Domain.Entites;
+
+namespace WebStore.Project.Backend.Infra.Mappings
+{
+    public class FornecedorMapConfiguration : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.ToTable("Fornecedores");
+
+            builder.Property(f => f.NomeFantasia)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(f => f.RazaoSocial)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(f => f.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(f => f.Cnpj)
+                .IsFixedLength()
+                .HasMaxLength(18)
+                .IsRequired();
+
+            builder.Property(f => f.Telefone)
+                .IsFixedLength()
+                .HasMaxLength(11)
+                .IsRequired();
+        }
+    }
+}
--- WebStore.Project/Startup.cs
+++ WebStore.Project/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebStore.Project.Backend.Infra;
 
 namespace WebStore.Project
 {
@@ -23,7 +18,6 @@ namespace WebStore.Project
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +28,6 @@ namespace WebStore.Project
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,16 +37,23 @@ namespace WebStore.Project
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.Project v1"));
             }
 
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope
+                    .ServiceProvider
+                    .GetRequiredService<MainContext>();
+                context
+                    .Database
+                    .Migrate();
+            }
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
